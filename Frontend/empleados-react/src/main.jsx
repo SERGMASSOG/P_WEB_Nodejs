f681017b17395c94befa03740370140d1487002e
SERGIO MASSO GIRALDO <sergio.masso@uao.edu.co>
2025-05-31 16:02:35 -0500

    Update main.jsx

--- Frontend/empleados-react/src/main.jsx
+++ Frontend/empleados-react/src/main.jsx
@@ -1,10 +1,11 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App.jsx";
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './components/App.jsx' // ✅ Ruta corregida
+import './index.css'
 
-createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
