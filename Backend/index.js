3b1c316709fb0224b174cbdce5b39e60bdbb6f2c
SERGIO MASSO GIRALDO <sergio.masso@uao.edu.co>
2025-05-31 14:26:05 -0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({origin: ['http://localhost:5173'], credentials: true }));
+app.use(cors({origin: ['http://localhost:5173', 'https://p-web-nodejs.onrender.com'], credentials: true }));
 
 // 🔹 Importar rutas
 const empleadoRoutes = require('./routes/empleado.route');
